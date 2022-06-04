e28b72bcf588c56e710c0f1d97d7e415c4f91b63
rachidassouani <rachidassouani@gmail.com>
2022-06-04 23:26:48 +0100

    Finished the customerRestController in order to CRUD the customer

---
+++ src/main/java/io/rachidassouani/ebankbackend/web/CustomerRestController.java
@@ -0,0 +1,39 @@
+package io.rachidassouani.ebankbackend.web;
+
+import io.rachidassouani.ebankbackend.dto.CustomerDTO;
+import io.rachidassouani.ebankbackend.exception.CustomerNotFoundException;
+import io.rachidassouani.ebankbackend.service.CustomerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("customers")
+public class CustomerRestController {
+
+    private final CustomerService customerService;
+
+    public CustomerRestController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<CustomerDTO> customersList() {
+        return customerService.findAll();
+    }
+
+    @GetMapping("{customerId}")
+    public CustomerDTO customerDTO(@PathVariable("customerId") long customerId) throws CustomerNotFoundException {
+        return customerService.findCustomerDTO(customerId);
+    }
+
+    @PostMapping
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        return customerService.save(customerDTO);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") long customerId) throws CustomerNotFoundException {
+        customerService.delete(customerId);
+    }
+}
